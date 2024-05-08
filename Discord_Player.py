315298f4c129eff28a4be8c151f4abcba8b035f7
DmitryFed <astrozzomb@gmail.com>
2024-05-08 17:45:35 +0300

    commens, udpdate gitignore

--- Discord_Player.py
+++ Discord_Player.py
@@ -122,10 +122,13 @@ def run_bot():
     ytdl = yt_dlp.YoutubeDL(yt_dl_options)
     ffmpeg_options = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options': '-vn -filter:a "volume=0.25"'}
 
+    #check that bot is running
     @client.event
     async def on_ready():
         print(f'{client.user} is now jamming')
+    
 
+    #play next song
     async def play_next(ctx):
        try:
             if queues[ctx.guild.id]!= []:
@@ -135,10 +138,10 @@ def run_bot():
                 await play(ctx, link)
        except Exception as e:
             print(e)
-
+    
+    #main command:play
     @client.command(name="play")
     async def play(ctx, link):
-       
         try:
             if ctx.author.voice is None: return await ctx.send("Get in a voice channel first...")
             voice_client = await ctx.author.voice.channel.connect()
@@ -147,7 +150,6 @@ def run_bot():
             print(e)
         
         try:
-       
             loop = asyncio.get_event_loop()
             data = await loop.run_in_executor(None,lambda: ytdl.extract_info(link, download=False))
             song = data['url']
@@ -162,7 +164,8 @@ def run_bot():
                voice_clients[ctx.guild.id].play(player, after=lambda e: asyncio.run_coroutine_threadsafe(play_next(ctx), client.loop))
         except Exception as e:
             print(e)
-
+    
+    #clear queue
     @client.command(name="clear_queue")
     async def clear_queue(ctx):
         if ctx.guild.id in queues:
@@ -170,7 +173,8 @@ def run_bot():
             await ctx.send("Queue cleared!")
         else:
             await ctx.send("There is no queue to clear!")
-
+    
+    #repeat queue
     @client.command(name = "repeat")
     async def repeat(ctx):
         global repeat_songs
@@ -181,7 +185,8 @@ def run_bot():
             await ctx.send("repeated now")
         else: 
             await ctx.send("don't repeated now")
-
+    
+    #pause bot
     @client.command(name="pause")
     async def pause(ctx):
         try:
@@ -189,13 +194,15 @@ def run_bot():
         except Exception as e:
             print(e)
    
+    #resume
     @client.command(name="resume")
     async def resume(ctx):
         try:
             voice_clients[ctx.guild.id].resume()
         except Exception as e:
             print(e)
-
+    
+    #skip
     @client.command(name="skip")
     async def skip(ctx):
         try:
@@ -204,7 +211,8 @@ def run_bot():
                await play_next(ctx)
         except Exception as e:
             print(e)
-
+    
+    #stops bot
     @client.command(name="stop")
     async def stop(ctx):
         try:
@@ -219,12 +227,14 @@ def run_bot():
         except Exception as e:
             print(e)
 
+    #add to queue
     async def queue(ctx, url):
         #if key does not exists
         if ctx.guild.id not in queues:
             queues[ctx.guild.id].append([])
         queues[ctx.guild.id][0].append(url)
     
+    #getting queue list 
     @client.command(name="get")
     async def get(ctx):
         if ctx.guild.id in queues:
@@ -232,7 +242,7 @@ def run_bot():
              await ctx.send(u)
 
     client.run(TOKEN)
-
+#global vars
 current_song = None
 repeat_songs = 0
 from_queue = 0
